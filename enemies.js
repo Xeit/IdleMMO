e98bf87bd981d21c01812e876b5e54e8b5838d6f
Piotr "Xeiciu" Lisowski <xeit@op.pl>
2024-08-30 00:24:39 +0200

    Alghoritm for enemies
    - Added a bit of automatic scaling for enemies as writing and balancing it by hand would be way too painful for me to consider. I'm too lazy so added something that can make it easier and faster.

--- enemies.js
+++ enemies.js
@@ -1,17 +1,28 @@
+const EnemyDifficulty =
+{
+	easy: 1,
+	medium: 1.5,
+	hard: 2,
+	very_hard: 3,
+	mini_boss: 5,
+	boss: 10
+};
+
 class Enemy
 {
-	constructor(enemyName, enemyLevel, enemyXp, enemyMaxHealth, enemyDamage, chanceOfDroppingItem)
+	constructor(enemyName, enemyLevel, chanceOfDroppingItem, enemyDiff)
 	{
 		this.enemyName = enemyName;
 		this.enemyLevel = enemyLevel;
-		this.enemyXp = enemyXp;
-		this.enemyMaxHealth = enemyMaxHealth;
-		this.enemyDamage = enemyDamage;
+		this.enemyDifficulty = enemyDiff;
+		this.enemyXp = +(enemyLevel * 2 * enemyDiff).toFixed();
+		this.enemyMaxHealth = +(enemyLevel * 15 * enemyDiff).toFixed();
+		this.enemyDamage = +(enemyLevel * 4 * enemyDiff).toFixed();
 		this.chanceOfDroppingItem = chanceOfDroppingItem;
 	}
 }
 
 const enemyMap = new Map();
 
-enemyMap.set("Rat", new Enemy("Rat", 1, 1, 10, 3, 15));
-enemyMap.set("Big Rat", new Enemy("Big Rat", 3, 5, 30, 6, 60));
+enemyMap.set("Rat", new Enemy("Rat", 1, 15, EnemyDifficulty.easy));
+enemyMap.set("Big Rat", new Enemy("Big Rat", 3, 60, EnemyDifficulty.medium));
