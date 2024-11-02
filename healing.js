e18d30ad54255ccc682f1431fbffdcd9ae1ecb76
Piotr "Xeiciu" Lisowski <xeit@op.pl>
2024-11-02 23:56:11 +0100

    Reworked healing
    - Healing should now be more predictable and balanced for all levels.
    - Updated version on website with new balance changes

--- healing.js
+++ healing.js
@@ -19,8 +19,15 @@ function tickHealing()
 {
 	if(bIsHealing)
 	{
-		playerHealth += playerStamina * 2;
-		playerMana += playerIntelligence * 2;
+		//Idea is that with maxes stamina stat on current level player heal 20% of hp per tick.
+		//If stamina is lower then heal percentage wise less
+		const maxPlayerStatLvl = playerLevel * 3;
+
+		const hpToHeal = Math.round(playerMaxHealth / 5 * (playerStamina / maxPlayerStatLvl));
+		const manaToHeal = Math.round(playerMaxMana / 5 * (playerIntelligence / maxPlayerStatLvl));
+
+		playerHealth += hpToHeal;
+		playerMana += manaToHeal;
 		if(playerHealth > playerMaxHealth)
 		{
 			playerHealth = +playerMaxHealth;
