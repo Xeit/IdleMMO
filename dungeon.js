ddb11ef9f74657f5b5cc0315b34ad3bd28517375
Piotr "Xeiciu" Lisowski <xeit@op.pl>
2025-09-04 00:14:20 +0200

    Dungeons - I don't know where I'm going with this

---
+++ dungeon.js
@@ -0,0 +1,14 @@
+class DungeonExperience
+{
+	teamwork = 0;
+	mechanics = 0;
+	gameKnowledge = 0; // This might be trown out
+
+	mechanicAwarness = 0; // This should be per dungeon
+}
+
+class Dungeon
+{
+	name = "";
+	monsters = [];
+}
